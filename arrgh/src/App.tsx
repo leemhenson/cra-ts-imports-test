1965ec0d985ecc5af7c33a0ee73bae4e3619eb5a
Lee Henson <lee.m.henson@gmail.com>
2019-08-27 13:41:26 +0100

    maybe it works

--- arrgh/src/App.tsx
+++ arrgh/src/App.tsx
@@ -2,6 +2,9 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import { makeHnng } from "hnng/src/thing";
+import { DooDaa } from "waat/src/thing";
+
+const doo: DooDaa = { name: "outer doodaa" };
 
 const App: React.FC = () => {
   return (
@@ -9,7 +12,7 @@ const App: React.FC = () => {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.tsx</code> and save to reload. {makeHnng()}
+          Edit <code>src/App.tsx</code> and save to reload. {makeHnng()} {doo.name}
         </p>
         <a
           className="App-link"
